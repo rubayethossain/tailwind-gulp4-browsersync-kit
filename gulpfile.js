5fa312f78696d39c540d548b7f6c3fa6980d9d7b
Md. Rubayet Hossain <rubayet33@gmail.com>
2020-03-16 01:54:57 +0600

    Removes Sass configuration, not needed now

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var { src, dest, series, watch, parallel } = require("gulp");
-// var sass = require("gulp-sass");
 var browserSync = require("browser-sync").create();
 var postcss = require("gulp-postcss");
 var tailwindcss = require("tailwindcss");
@@ -19,15 +18,7 @@ function tailwind() {
     .pipe(dest("app/css/"));
 }
 
-// function scssCompiler() {
-//   return src("./app/scss/**/*.scss")
-//     .pipe(sass())
-//     .pipe(dest("./app/css"))
-//     .pipe(browserSync.stream());
-// }
-
 function watchTasks() {
-  // watch("./app/scss/**/*.scss", scssCompiler);
   watch("app/css/tailwind/styles.css", tailwind);
   watch("app/**/*.html").on("change", browserSync.reload);
   watch("app/**/*.js").on("change", browserSync.reload);
